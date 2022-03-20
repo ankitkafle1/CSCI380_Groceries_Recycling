65c3d6c3fd773b5fde098b39afcc578dd35d0a61
Ankit69 <51173448+Ankit69@users.noreply.github.com>
2022-03-20 13:24:05 -0400

    modified backend filse, added some routes to fetch data from express server,

--- helping_hands/src/App.js
+++ helping_hands/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { useState, useEffect } from 'react';
+import Header from './components/header_footer/Header';
+import AddCustomer from './components/customers/AddCustomer';
+//import Profile from './components/header_footer/Profile';
+import Customers from './components/customers/Customers';
+import apiRequest from './components/ApiReq';
+import ApiRequest from './components/ApiReq';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const API_URL = 'http://localhost:4000/api/customers';
+  const [CustomersInfo,setCustomers] = useState([]);
 
+  useEffect(() => {
+    const fetchItems = async() => {
+        const response = await fetch(API_URL);
+        const customersList = await response.json();
+        setCustomers(customersList)
+    }
+    fetchItems();
+    }, [])
+
+    const addItems = async (item) => {
+      const id = 10
+      const myNewItem = {'name':"i am a disco dancer"};
+      const listItem = [...CustomersInfo,myNewItem];
+      setCustomers(listItem)
+      const postOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(myNewItem)
+      }
+      const result = await apiRequest(API_URL, postOptions);
+    }
+    
+  return(
+    <div>
+        <Header/>
+        <Customers CustomersInfo ={CustomersInfo}/>
+        <AddCustomer 
+        CustomersInfo={CustomersInfo}
+        //addItems={addItems}
+        />
+    </div>
+    )
+}
 export default App;
